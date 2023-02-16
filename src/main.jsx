2d899554cf864e69bae51a9cfb343f62616eb9d6
Jason Zhao <jasonnope123@gmail.com>
2023-02-15 19:32:51 -0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Register />
+  </React.StrictMode>,
+)
