1387efea047dc5a02aa3ca26da9ceb755760859b
zjzn <jasonnope123@gmail.com>
2023-02-15 22:47:00 -0800

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
   return (
     <div className='h-28 bg-white flex justify-between items-center'>
         <span className='pl-5 text-bg font-bold text-3xl'>Alpha Chat</span>
-        <div className='flex gap-10 pr-5'>
-            <span>Jason Zhao</span>
-            <button className='bg-bg'>Logout</button>
+        <div className='flex gap-5 pr-5 items-center'>
+            <span className='text-gray-500 font-medium'>Jason Zhao</span>
+            <button className='bg-bg p-3 text-white rounded-md hover:scale-110 duration-300'>Logout</button>
         </div>
 
     </div>
