1bf6a0d810a44f906e2c9a67c4e81f6edf4dc31f
zjzn <jasonnope123@gmail.com>
2023-02-16 19:57:59 -0800

    chat component

---
+++ src/components/Chats.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Chats = () => {
+  return (
+    <div>
+        <div className='p-10 flex items-center gap-10 bg-gray-500 cursor-pointer'>
+            <img src='https://cdn-icons-png.flaticon.com/512/3884/3884851.png' className='w-12 h-12 rounded-full'></img>
+            <div>
+                <span className='text-lg font-medium text-white '>Jason Zhao</span>
+                <p className='text-base text-gray-200'>Test</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Chats
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Chats from './Chats'
 import Navbar from './Navbar'
+
 const Sidebar = () => {
   return (
     <div className='flex-none w-1/4 h-screen bg-black'>
         <Navbar />
-        
-        <p1>sidebar</p1>
+        <Chats/>
     </div>
   )
 }
