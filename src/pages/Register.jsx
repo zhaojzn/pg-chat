a8906a96e0f2879868b9cce168686441123361d5
Jason Zhao <jasonnope123@gmail.com>
2023-02-26 23:27:06 -0800

    weird fix

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../firebase'
 const Register = () =>{
 
