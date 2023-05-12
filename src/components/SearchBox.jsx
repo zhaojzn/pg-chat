68eb9f07d925a3586f20481df9a82bfe43242cdd
Jason Zhao <jasonnope123@gmail.com>
2023-05-12 12:18:22 -0700

    c

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,16 +4,27 @@ import { ToastContainer, toast } from "react-toastify";
 import { db } from '../firebase'
 import { async } from '@firebase/util';
 import { AuthContext } from '../context/AuthContext';
-  
+import { useDebounce, useDebouncedCallback } from 'use-debounce';
 
 const SearchBox = () => {
 
 
   const [search, setSearch] = useState("")
-  const handleKeyDown = (e) => {
-      console.log(search)
-  }
+  const [value, setValue] = useState("")
 
+  const debonced = useDebouncedCallback(
+    
+    (value) => {
+      console.log(value + " - debounced")
+      setValue(value)
+    }
+    ,
+    800
+  );
+
+
+
+  
   return (
     <div className='bg-black items-center justify-center w-1/2 h-3/4 absolute top-[10%] right-1/4 opacity-100'>
         <div className='w-full h-[10%] bg-bg items-center flex justify-center'>
@@ -21,9 +32,7 @@ const SearchBox = () => {
             placeholder="Search username" 
             className="bg-transparent 
             text-white outline-0 w-100 text-center"
-            onChange={e=>setSearch(e.target.value)}
-            onKeyDown={handleKeyDown}
-            value={search} 
+            onChange={e=>debonced(e.target.value)}
             />
         </div>
     </div>
