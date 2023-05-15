59d5748d734d61cf3a18cf761116bb51e47d86dc
Jason Zhao <jasonnope123@gmail.com>
2023-05-15 12:30:20 -0700

    Search final

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -5,35 +5,12 @@ import { db } from '../firebase'
 import { async } from '@firebase/util';
 import { AuthContext } from '../context/AuthContext';
 import { useDebounce, useDebouncedCallback } from 'use-debounce';
-<<<<<<< HEAD
-=======
   
->>>>>>> origin/master
 
 const SearchBox = () => {
 
-
-<<<<<<< HEAD
-  const [search, setSearch] = useState("")
-  const [value, setValue] = useState("")
-
-  const debonced = useDebouncedCallback(
-    
-    (value) => {
-      console.log(value + " - debounced")
-      setValue(value)
-    }
-    ,
-    800
-  );
-
-
-
-  
-=======
-
   const [search, setSearch] = useState("");
-  const [user, setUser] = useState();
+  const [user, setUser] = useState([]);
   const [err, setErr] = useState();
 
   const debounced = useDebouncedCallback((search) => {
@@ -42,53 +19,53 @@ const SearchBox = () => {
   }, 1000);
 
   //handle 
-  const handleApiCall = async (search) =>{
-    const q = query(collection(db, "users"), where("displayName", "==", search));
-    try{
+  const handleApiCall = async (search) => {
+    console.log("debouced -" + search);
+    const q = query(
+      collection(db, "users"),
+      where("displayName", ">=", search),
+      where("displayName", "<=", search + "\uf8ff") // add a range filter to match names that start with search
+    );
+    try {
       const querySnapshot = await getDocs(q);
-      console.log("1" + "-  " + JSON.stringify(querySnapshot))
-      if(querySnapshot.docs.length > 0){
-        setUser(querySnapshot)
-      }else{
-          setUser("")
+      console.log(querySnapshot);
+      if (querySnapshot.docs.length > 0) {
+        const users = [];
+        querySnapshot.forEach((doc) => {
+          users.push(doc.data());
+          console.log(doc.id, " => ", doc.data());
+        });
+        setUser(users);
+      } else {
+        setUser([]);
       }
-
-    //snapshot each query
-
-    }catch (e){
-      console.log("Error")
+    } catch (e) {
+      console.log(e);
     }
-    console.log(user)
-  }
+    console.log(user);
+  };
 
 
-
->>>>>>> origin/master
   return (
-    <div className='bg-black items-center justify-center w-1/2 h-3/4 absolute top-[10%] right-1/4 opacity-100'>
+    <div className='bg-black items-center justify-center w-1/2 h-3/4 absolute top-[10%] right-1/4 opacity-100 overflow-scroll'>
         <div className='w-full h-[10%] bg-bg items-center flex justify-center'>
             <input type="text" 
             placeholder="Search username" 
             className="bg-transparent 
             text-white outline-0 w-100 text-center"
-<<<<<<< HEAD
-            onChange={e=>debonced(e.target.value)}
-=======
             onChange={(e) => debounced(e.target.value)}
->>>>>>> origin/master
             />
         </div>
-        {user && Object.entries(user).map((m) => (
-          <div>
-            <div className='p-10 flex justify-center items-gap-10 bg-gray-500 cursor-pointer hover:bg-gray-400 rounded-full w-full' key={m}>
-            <span className='text-lg font-medium text-white '></span>
-
+          {user && user.map((u) => (
+          <div key={u.displayName}>
+            <div className='p-10 flex justify-center items-gap-10 bg-gray-500 cursor-pointer hover:bg-gray-400 rounded-full w-full'>
+              <span className='text-lg font-medium text-white'>{u.displayName}</span>
             </div>
           </div>
-        ))} 
+        ))}
 
     </div>
   )
 }
 
-export default SearchBox
+export default SearchBox  
